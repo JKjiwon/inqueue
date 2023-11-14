ea353263c6e7336c0f5a5319b91ff2681d2bb6ab
JKjiwon <jwkim.oa@gmail.com>
2023-11-14 19:37:06 +0900

    feat : 로컬 환경에서 EmbeddedRedis

---
+++ src/main/kotlin/com/flab/inqueue/infra/config/DevEmbeddedRedisConfig.kt
@@ -0,0 +1,30 @@
+package com.flab.inqueue.infra.config
+
+import com.flab.inqueue.infra.property.RedisProperty
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import redis.embedded.RedisServer
+
+@Profile("dev")
+@Configuration
+class DevEmbeddedRedisConfig (
+    redisProperty: RedisProperty
+) {
+    private val redisServer: RedisServer
+
+    init {
+        redisServer = RedisServer(redisProperty.port)
+    }
+
+    @PostConstruct
+    fun startRedis() {
+        redisServer.start()
+    }
+
+    @PreDestroy
+    fun stopRedis() {
+        redisServer.stop()
+    }
+}
