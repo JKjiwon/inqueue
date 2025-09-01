509e4c06fcf25b0b116bc1ec96423cdf7e44ce9a
kimjiwon <jwkim.oa@gmail.com>
2025-09-01 22:08:49 +0900

    chore: ddl 변경 - varchar 사이즈 변경

--- scripts/mysql/init.sql
+++ scripts/mysql/init.sql
@@ -4,17 +4,17 @@ CREATE TABLE member
     id                 BIGINT AUTO_INCREMENT PRIMARY KEY,
     created_date_time  DATETIME(6),
     modified_date_time DATETIME(6),
-    client_id          VARCHAR(255),
-    client_secret      VARCHAR(255),
-    name               VARCHAR(255),
-    phone              VARCHAR(255)
+    client_id          VARCHAR(50),
+    client_secret      VARCHAR(100),
+    name               VARCHAR(30),
+    phone              VARCHAR(20)
 );
 
 -- member_role 테이블
 CREATE TABLE member_role
 (
     member_id BIGINT NOT NULL,
-    role      VARCHAR(255),
+    role      VARCHAR(30),
     CONSTRAINT fk_member_role_member FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
@@ -24,7 +24,7 @@ CREATE TABLE event
     id                   BIGINT AUTO_INCREMENT PRIMARY KEY,
     created_date_time    DATETIME(6),
     modified_date_time   DATETIME(6),
-    event_id             VARCHAR(255) NOT NULL,
+    event_id             VARCHAR(50) NOT NULL,
     job_queue_limit_time BIGINT       NOT NULL,
     job_queue_size       BIGINT       NOT NULL,
     end_date_time        DATETIME(6) NOT NULL,
